fa8ab5a909d8a775890da96ee74acb9b7498733c
nafilah17 <rissanafilah@gmail.com>
2023-09-06 09:58:01 +0700

    add button navbar

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -42,7 +42,17 @@ export default function Navbar() {
                 </a>
               </li>
             </ul>
-           
+            <div className="buttons">
+              <a href="" className="btn btn-outline-dark">
+                <i className="fa fa-sign-in me-1"></i> Login
+              </a>
+              <a href="" className="btn btn-outline-dark ms-2">
+                <i className="fa fa-user-plus me-1"></i> Register
+              </a>
+              <a href="" className="btn btn-outline-dark ms-2">
+                <i className="fa fa-shopping-cart me-1"></i> Cart (0)
+              </a>
+            </div>
           </div>
         </div>
       </nav>
