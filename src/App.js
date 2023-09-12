05256af29d6988625e61ab208c8c9752eaf01c47
nafilah17 <rissanafilah@gmail.com>
2023-09-12 10:44:34 +0700

    configuration of navbar and filter products

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import React from 'react';
-import './App.css';
-import Navbar from './component/Navbar';
-import Home from './component/Home';
+import React from "react";
+import "./App.css";
+import Navbar from "./component/Navbar";
+import Home from "./component/Home";
+import Products from "./component/Products";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
-    <Navbar />
-    <Home />
-    
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/products" element={<Products />} />
+      </Routes>
     </>
   );
 }
